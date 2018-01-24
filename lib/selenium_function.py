58757e78a41b16c7c642a6fe61ef20c052676b6a
Koichi Taura <taura.koichi@gmail.com>
2018-01-24 23:58:06 +0900

    add login function

--- lib/selenium_function.py
+++ lib/selenium_function.py
@@ -13,3 +13,13 @@ class SeleniumFunction:
 
     def get(self, url):
         self.driver.get(url)
+
+    def login(self, account, password, account_class, password_class, submit_class):
+        account_input = self.driver.find_element_by_class_name(account_class)
+        password_input = self.driver.find_element_by_class_name(password_class)
+        submit_button = self.driver.find_element_by_class_name(submit_class)
+
+        account_input.send_keys(account)
+        password_input.send_keys(password)
+
+        submit_button.submit()
