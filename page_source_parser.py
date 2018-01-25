58a2618725bb8c73ac0d69c1b013ca3838d6b6b0
Koichi Taura <taura.koichi@gmail.com>
2018-01-25 23:19:22 +0900

    add page parser tool

---
+++ page_source_parser.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+from bs4 import BeautifulSoup
+
+from entity.web_page_entity import WebPage
+
+
+class PageSourceParser:
+    def __init__(self, url, page_source):
+        self.page_source = page_source
+        soup = BeautifulSoup(page_source, "html.parser")
+        title = soup.find("title")
+
+        WebPage(
+            title=title,
+            url=url,
+        ).save()
