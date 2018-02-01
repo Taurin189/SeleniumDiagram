b2cde64e3df8248d12a2aa5a715495dad1bb3edf
Koichi Taura <taura.koichi@gmail.com>
2018-02-01 23:42:18 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SeleniumDiagram',
+      version='1.0',
+      description='Make Diagram using selenium tool',
+      author='Koichi Taura',
+      author_email='taura.koichi@gmail.com',
+      url='https://github.com/Taurin189/SeleniumDiagram',
+      packages=['SeleniumDiagram', ''],
+     )
